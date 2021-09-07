576cfea87774e1f23f797bb7342e1ce33b40e297
David Andrs <andrsd@gmail.com>
2021-09-07 07:33:50 -0600

    Bump spotipy version to 2.19.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'PyQt5==5.15.2',
         'pyaml==20.4.0',
-        'spotipy==2.16.1',
+        'spotipy==2.19.0',
         'Flask==2.0.1',
         'waitress==1.4.4',
         'python-dotenv>=0.15.0'
